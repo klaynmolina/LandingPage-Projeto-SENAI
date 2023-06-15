0adc31c38f5f04d8d69c33e132796ad37332c992
Molina <83372957+klaynmolina@users.noreply.github.com>
2023-06-14 23:40:40 -0300

    update js

--- script.js
+++ script.js
@@ -2,20 +2,20 @@ const inicio = document.getElementById('inicio');
 const contato = document.getElementById('contato');
 
 window.onload = function () {
-    changeColorInicio();
     changeColorContato();
+    changeColorInicio();
 }
 
 function changeColorInicio() {
     const currentPage = 'current-page';
-    if (window.location.href.split('/')[3] == 'index.html') {
+    if (window.location.href.split('/')[4] == 'projeto-frontend-senai' || window.location.href.split('/')[5] == 'index.html') {
         inicio.classList.toggle(currentPage);
     } 
 }
 
 function changeColorContato() {
     const currentPage = 'current-page';
-    if (window.location.href.split('/')[3] == 'contato.html') {
+    if (window.location.href.split('/')[5] == 'contato.html') {
         contato.classList.toggle(currentPage);
-    } 
+    }
 }
